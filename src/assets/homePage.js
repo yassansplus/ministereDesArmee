051765ae538d64b2b145622a0baa94c13247c829
Yassine Bousaidi <34783077+yassansplus@users.noreply.github.com>
2021-03-16 10:33:40 +0100

    Archi/intégration vuejs (#1)
    * 👷🏽‍♂️: Intégration de vueJS, création d'un controller de base. Intégration du fichier JS homePage.js
    
    * 👷🏽‍♂️: Intégration de vueJS, création d'un controller de base. Intégration du fichier JS homePage.js
    
    * 👷🏽‍♂️: Intégration de vueJS, ajout d'exemple de components réutilisable.

---
+++ src/assets/homePage.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+
+import DemoComponent from "./Component/Demo";
+
+new Vue({
+    el: "#demoComponent",
+    components: { DemoComponent },
+    template: "<DemoComponent />",
+
+
+})
