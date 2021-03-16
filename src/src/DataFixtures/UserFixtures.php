8a1c73b5a522925faec3a8fdd627043339333bca
Nolikein <wasik.come@gmail.com>
2021-03-16 11:50:00 +0100

    :sparkles: add user entity and login guard/form

---
+++ src/src/DataFixtures/UserFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    /** @var UserPasswordEncoderInterface $passwordEncoder A password encoder */
+    protected $passwordEncoder = null;
+
+    /**
+     * Inject services
+     * 
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     */
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+
+        $user = new User();
+        $user = $user->setUsername('admin')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword($this->passwordEncoder->encodePassword($user, 'password'));
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
